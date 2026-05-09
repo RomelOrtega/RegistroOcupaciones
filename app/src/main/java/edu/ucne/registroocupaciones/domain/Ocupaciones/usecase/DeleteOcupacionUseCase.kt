2eec7334a553eea029edb0133e34fd0a1e7f8944
RomelOrtega <romel_ortega@ucne.edu.do>
2026-05-08 20:46:36 -0400

    Se creo DeleteOcupacionUseCase

---
+++ app/src/main/java/edu/ucne/registroocupaciones/domain/Ocupaciones/usecase/DeleteOcupacionUseCase.kt
@@ -0,0 +1,13 @@
+package edu.ucne.registroocupaciones.domain.Ocupaciones.usecase
+
+import edu.ucne.registroocupaciones.domain.Ocupaciones.repository.OcupacionRepository
+import javax.inject.Inject
+
+class DeleteOcupacionUseCase @Inject constructor(
+    private val repository: OcupacionRepository
+) {
+    suspend operator fun invoke(id: Int) {
+        if (id <= 0) throw IllegalArgumentException("El ID debe ser mayor que 0")
+        repository.delete(id)
+    }
+}
