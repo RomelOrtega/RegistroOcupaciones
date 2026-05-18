d78bd28ab37e899e5d654dc921c198398ed0c768
RomelOrtega <romel_ortega@ucne.edu.do>
2026-05-17 23:19:02 -0400

    Se agrega la funcion empleadoDao a la Db

--- app/src/main/java/edu/ucne/registroocupaciones/data/database/OcupacionDb.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/database/OcupacionDb.kt
@@ -4,14 +4,18 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.registroocupaciones.data.local.dao.OcupacionDao
 import edu.ucne.registroocupaciones.data.local.entities.OcupacionEntity
+import edu.ucne.registroocupaciones.data.local.dao.EmpleadoDao
+import edu.ucne.registroocupaciones.data.local.entities.EmpleadoEntity
 
 @Database(
     entities = [
         OcupacionEntity::class,
+        EmpleadoEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class OcupacionDb : RoomDatabase() {
     abstract fun ocupacionDao(): OcupacionDao
+    abstract fun empleadoDao(): EmpleadoDao
 }
